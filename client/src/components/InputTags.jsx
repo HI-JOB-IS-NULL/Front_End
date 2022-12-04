f9d4d9104f427b90e80ba4b9d5e3e08a48462437
baghestani shaghayegh <sara7space@gmail.com>
2022-12-04 18:01:35 +0900

    Meal Planner page ui

--- client/src/components/InputTags.jsx
+++ client/src/components/InputTags.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-
+import IngredientsData from "../IngredientsData";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 export default function InputTags(props) {
   const [tags, setTags] = useState([]);
+  const suggestions = IngredientsData.map((item) => item.name);
+  const [isFocused, setIsFocused] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
 
   const addTags = (event) => {
     if (event.target.value != "") {
@@ -15,7 +20,13 @@ export default function InputTags(props) {
 
   const removeTags = (indexToRemove) => {
     setTags([...tags.filter((_, index) => index != indexToRemove)]);
+    props.selected([...tags]);
   };
+
+  function addTagsFromSuggestion(suggestion) {
+    setTags([...tags, suggestion]);
+    props.selected([...tags, suggestion]);
+  }
   return (
     <Container>
       <div className="tags-input-wrapper">
@@ -32,12 +43,55 @@ export default function InputTags(props) {
             </li>
           ))}
         </ul>
-        <input
-          type="text"
-          placeholder="Exclude Ingredients"
-          className="tags-input"
-          onKeyUp={(e) => (e.key === "Enter" ? addTags(e) : null)}
-        />
+
+        <div className="tags-input">
+          <div>
+            <input
+              type="text"
+              placeholder="Exclude Ingredients"
+              className="suggestion-input"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => {
+                if (!isHovered) {
+                  setIsFocused(false);
+                }
+              }}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyUp={(e) => (e.key === "Enter" ? addTags(e) : null)}
+            />
+          </div>
+
+          {isFocused && (
+            <div
+              className="suggestion-wrapper"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              {suggestions.map((suggestion, index) => {
+                const isMatch =
+                  suggestion
+                    .toLocaleLowerCase()
+                    .indexOf(inputValue.toLocaleLowerCase()) > -1;
+                return (
+                  <div key={index}>
+                    {isMatch && (
+                      <div
+                        className="suggestion"
+                        onClick={() => addTagsFromSuggestion(suggestion)}
+                      >
+                        <AddCircleOutlineOutlinedIcon
+                          fontSize="small"
+                          style={{ color: "green" }}
+                        />
+                        {suggestion}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
       </div>
     </Container>
   );
@@ -46,6 +100,7 @@ export default function InputTags(props) {
 const Container = styled.div`
   .tags-input-wrapper {
     display: flex;
+    position: relative;
     align-items: center;
     gap: 5px;
     flex-wrap: wrap;
@@ -80,13 +135,38 @@ const Container = styled.div`
     }
 
     .tags-input {
-      flex: 1;
-      border: none;
-      height: 46px;
-      font-size: 14px;
-      padding: 4px 0 0 0;
-      &:focus {
-        outline: transparent;
+      display: flex;
+      flex-direction: column;
+      position: relative;
+      .suggestion-input {
+        border: none;
+        outline: none;
+        flex: 1;
+        height: 46px;
+        font-size: 14px;
+        padding: 4px 0 0 0;
+        background: transparent;
+      }
+
+      .suggestion-wrapper {
+        box-shadow: 0 0 14px rgb(0 0 0 / 8%);
+        max-height: 150px;
+        overflow-y: auto;
+        width: 480px;
+        background: white;
+        position: absolute;
+        top: 50px;
+        left: -10px;
+
+        .suggestion {
+          display: flex;
+          align-items: center;
+          gap: 3px;
+          cursor: pointer;
+          &:hover {
+            background-color: #f2f3f4;
+          }
+        }
       }
     }
   }
