85c8e0653a1e9d251a96f4e5f2653f4047f91dd6
baghestani shaghayegh <sara7space@gmail.com>
2022-12-09 16:31:19 +0900

    fixed check problem for diet filter

--- client/src/components/GridChoices.jsx
+++ client/src/components/GridChoices.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+
 export default function GridChoices(props) {
   const [isSelected, setIsSelected] = useState([]);
   console.log(isSelected);
+  const removeFromIsSelected = (itemToRemove) => {
+    console.log("remove item from isSelected");
+    setIsSelected(isSelected.filter((element) => element !== itemToRemove));
+  };
+
   return (
     <Container>
       <div className="grid-group">
@@ -13,21 +19,32 @@ export default function GridChoices(props) {
               key={index}
               onClick={() => {
                 props.multiple
-                  ? !isSelected.includes(item) &&
-                    setIsSelected((prevState) => [...prevState, item])
+                  ? !isSelected.includes(item)
+                    ? setIsSelected((prevState) => [...prevState, item])
+                    : removeFromIsSelected(item)
                   : setIsSelected(item);
               }}
             >
               <span
-                className={`${
-                  isSelected.includes(item) ? "color--green" : ""
-                } item-span`}
+                className={
+                  props.multiple
+                    ? `${
+                        isSelected.includes(item) ? "color--green" : ""
+                      } item-span`
+                    : `${isSelected === item ? "color--green" : ""} 
+                item-span`
+                }
               >
                 {item}
               </span>
-              {isSelected.includes(item) && (
-                <CheckOutlinedIcon style={{ color: "green" }} />
-              )}
+
+              {Array.isArray(isSelected)
+                ? isSelected.find((element) => element === item) && (
+                    <CheckOutlinedIcon style={{ color: "green" }} />
+                  )
+                : isSelected === item && (
+                    <CheckOutlinedIcon style={{ color: "green" }} />
+                  )}
             </div>
           );
         })}
